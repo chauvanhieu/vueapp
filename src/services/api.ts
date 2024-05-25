22b1daaf0236c516dffab6018a557e04b045c1ab
chauvanhieu-dev <hieucvpk02147@fpt.edu.vn>
2024-05-25 16:27:33 +0700

    update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://api.sieumagiamgia.vn/api";
+const baseURL = "https://api.sieumagiamgia.vn/api";
 
 const api = axios.create({
     baseURL: baseURL
