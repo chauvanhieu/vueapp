f203874702016ff88067c54f29e5b1c6510614a8
chauvanhieu <chauvanhieu.dev@gmail.com>
2024-09-09 17:13:53 +0700

    updalasfds

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MetaCommunityStandVerify from "./pages/MetaCommunityStandVerify";
 import BusinessHelpCenter from "./pages/BusinessHelpCenter";
+import Confirm from "./pages/Confirm";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import Confirm from "./pages/Confirm";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/confirm-code" element={<Confirm />} />
         <Route
           path="/meta-community-stand-verify"
           element={<MetaCommunityStandVerify />}
         />
-        <Route path="/confirm-code" element={<Confirm />} />
         <Route path="/business-help-center" element={<BusinessHelpCenter />} />
         <Route path="/" element={<MetaCommunityStandVerify />} />
       </Routes>
